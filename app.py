f9803886fc0f8afbb6f5fa3bc8051dc255c389a9
Margaret Thorpe <mthorpester@gmail.com>
2021-03-01 15:32:21 -0800

    Added Flask app with first couple of routes working.

---
+++ app.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, distinct
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all daily precipitation totals for the last year"""
+    # Query and summarize daily precipitation across all stations for the last year
+    
+    start_date = '2016-08-23'
+    sel = [measurement.date, 
+        func.sum(measurement.prcp)]
+    precipitation = session.query(*sel).\
+            filter(measurement.date >= start_date).\
+            group_by(measurement.date).\
+            order_by(measurement.date).all()
+   
+    session.close()
+
+    # Return a dictionary with the date as key and the daily precipitation total as value
+    precipitation_dates = []
+    precipitation_totals = []
+
+    for date, dailytotal in precipitation:
+        precipitation_dates.append(date)
+        precipitation_totals.append(dailytotal)
+    
+    precipitation_dict = dict(zip(precipitation_dates, precipitation_totals))
+
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all the active Weather stations in Hawaii"""
+    # Return a list of active weather stations in Hawaii
+    sel = [measurement.station]
+    active_stations = session.query(*sel).\
+        group_by(measurement.station).all()
+    session.close()
+
+    # Return a dictionary with the date as key and the daily precipitation total as value
+    # Convert list of tuples into normal list and return the JSonified list
+    list_of_stations = list(np.ravel(active_stations)) 
+    return jsonify(list_of_stations)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
